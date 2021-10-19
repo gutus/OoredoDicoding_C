94261377bfec996a0fe20326caa2b3cb12fd5bea
gutus <gutusnirwanto@gmail.com>
2021-10-19 09:53:40 +0700

    master

---
+++ Bab1/06_assign_float.c
@@ -0,0 +1,19 @@
+/* Assign Float */
+#include <stdio.h>
+int main()
+{ /* Kamus */
+    float f = 0.555;
+    float x = 1.5E3;
+
+    /* Algoritma  Nilai X*/
+    printf("Ini adalah nilai x \n");
+    printf("Ini nilai x : %f \n", x);
+    printf("Ini nilai x : %5.2f \n", x);
+    printf("Ini nilai x : %10.3f \n\n\n", x);
+    /* Algoritma  Nilai F*/
+    printf("Ini adalah nilai f \n");
+    printf("Ini nilai f : %f \n", f);
+    printf("Ini nilai f : %5.2f \n", f);
+    printf("Ini nilai f : %10.2f \n", f);
+    return 0;
+}
