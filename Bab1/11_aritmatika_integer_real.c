6db3918a5f5827f68316ee860df839bc7223524a
gutus <gutusnirwanto@gmail.com>
2021-11-12 13:31:10 +0700

    master

--- Bab1/11_aritmatika_integer_real.c
+++ Bab1/11_aritmatika_integer_real.c
@@ -11,6 +11,10 @@ int main()
     printf("Ini nilai x / y  : %d \n", x / y);
     printf("Ini nilai y / x  : %d \n", y / x);
     printf("Ini nilai x mod  y  : %d \n", x % y);
+    printf("Excerise 1 \n");
+    printf("Ini nilai x + x + x  : %d \n", x + x + x);
+    printf("Ini nilai x * x + x  : %d \n", x * x + x);
+    printf("Ini nilai (((1+2)/(7*5)*8)-7)+9  : %5.2d \n", (((1 + 2) / (7 * 5) * 8) - 7) + 9);
 
     /* bandingkan dengan operasi pembagian bilangan riil */
     float a = 5, b = 8;
