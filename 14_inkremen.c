40c4c72bb7b467ab1f6c8d0154ac0b7de520d136
gutus <gutusnirwanto@gmail.com>
2021-11-12 13:51:02 +0700

    master

---
+++ 14_inkremen.c
@@ -0,0 +1,14 @@
+/* Increment Operatiion */
+#include <stdio.h>
+int main()
+{
+    /* Kamus */
+    int i;
+    /* Program */
+    i = 3;
+    printf("Nilai i :%d %d\n", i, i++);
+    i = 3;
+    printf("%d\n", ++i); /* sebelum dicetak, nilai i ditambah 1 */
+
+    return 0;
+}
